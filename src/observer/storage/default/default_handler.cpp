2189960982705cabd7c6a94b4d03e43c769ae464
glq <1792990176@qq.com>
2023-10-27 09:11:58 +0800

    drop table 实现

--- src/observer/storage/default/default_handler.cpp
+++ src/observer/storage/default/default_handler.cpp
@@ -170,7 +170,11 @@ RC DefaultHandler::create_table(
 
 RC DefaultHandler::drop_table(const char *dbname, const char *relation_name)
 {
-  return RC::UNIMPLENMENT;
+  Db *db = find_db(dbname);  // 这是原有的代码，用来查找对应的数据库，不过目前只有一个库
+  if(db == nullptr) {
+    return RC::SCHEMA_DB_NOT_OPENED;
+  }
+  return db->drop_table(relation_name); // 直接调用db的删掉接口
 }
 
 Db *DefaultHandler::find_db(const char *dbname) const
